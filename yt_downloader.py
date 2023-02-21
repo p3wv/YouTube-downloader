8bd8541b15a8ff5a05c78aeeeb0a6e16e15645cf
p3wv <memiarzyskrzynia@gmail.com>
2023-02-22 00:32:46 +0100

    added menu allowing to choose the desired download format

--- yt_downloader.py
+++ yt_downloader.py
@@ -1,5 +1,5 @@
 from pytube import YouTube
-
+import os
 
 def get_usr_input():
 
@@ -7,7 +7,23 @@ def get_usr_input():
 
     return usr_given_video_link
 
-def video_download_by_high_res(yt_link):
+
+def download_only_audio(yt_link):
+
+    yt = YouTube(yt_link)
+
+    stream = yt.streams.get_audio_only()
+
+    audio_file = stream.download()
+
+    base, ext = os.path.splitext(audio_file)
+    audio_converted = base + '.mp3'
+    os.rename(audio_file, audio_converted)
+    
+    print("Audio from chosen video downloaded to current working directory!")
+
+
+def download_by_high_res(yt_link):
 
     yt = YouTube(yt_link)
 
@@ -15,7 +31,27 @@ def video_download_by_high_res(yt_link):
 
     stream.download()
 
+    print("Chosen video in the highest resolution downloaded to current working directory!")
+
+def menu():
+    submenu_selection = int(input("Choose video download classification: \n1. Audio\n2. Resolution\n\n"))
+
+    if submenu_selection == 1:
+        download_only_audio(get_usr_input())
+        
+    elif submenu_selection == 2:
+        download_by_high_res(get_usr_input())
+    else:
+        raise KeyError("Choose 1 or 2")
+
+# class Menu:
+#     def __init__(self) -> None:
+#         pass
+
+# class Resolution:
+#     def __init__(self) -> None:
+
+        
 
 if __name__ == '__main__':
-
-    video_download_by_high_res(get_usr_input())
+    menu()
